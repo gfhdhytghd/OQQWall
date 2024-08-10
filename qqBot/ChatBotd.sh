a88ef78d7d658db4d3c505faa18ddedbfe215896
gfhdhytghd <3391146750@qq.com>
2024-08-10 12:09:28 +0800

    more

--- qqBot/ChatBotd.sh
+++ qqBot/ChatBotd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source ./venv/bin/activate
 commgroup_id=$(grep 'communicate-group' oqqwall.config | cut -d'=' -f2 | tr -d '"')
+source ./venv/bin/activate
 echo $commgroup_id
 waitforfilechange(){
         last_mod_time_cmd=$(stat -c %Y "$1")
@@ -17,19 +17,17 @@ waitforfilechange(){
         fi
     done
 }
-sendmsggroup(){
-    google-chrome-stable --headless --screenshot 'http://127.0.0.1:8083/send_group_msg?group_id='$commgroup_id='&message='$1''
-}
 
 while true; do
     echo 启动等待循环
-    waitforfilechange "./getmsgserv/all/commugroup.txt"
-    mapfile -t lines < "$command_file"
-        line=${lines[-1]}
+    waitforfilechange ./getmsgserv/all/commugroup.txt
+    mapfile -t lines < "./getmsgserv/all/commugroup.txt"
+        question=${lines[-1]}
+        echo $question
         # 获取行的第一个和第二个字段
-        question=$(echo $line | awk '{print $1}')
-        botcmd = python3 /home/wilf/data/OQQWall/qqBot/ChatBot.py $question
+        botcmd="python3 ./qqBot/ChatBot.py $question"
         botoutput=$(eval $botcmd)
-        cmd="google-chrome-stable --headless --screenshot 'http://127.0.0.1:8083/send_group_msg?group_id='$commgroup_id='&message='$botoutput''"
+        cmd="google-chrome-stable --headless --screenshot 'http://127.0.0.1:8083/send_group_msg?group_id='$commgroup_id'&message='$botoutput'"
+        echo $cmd
         eval $cmd
 done
