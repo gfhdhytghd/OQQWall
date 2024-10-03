84fc73ae991c4e469bfa572775f10963f24edd79
gfhdhytghd <3391146750@qq.com>
2024-10-03 21:05:24 +0800

    more

--- qqBot/likeeveryday.py
+++ qqBot/likeeveryday.py
@@ -12,9 +12,9 @@ friend_list = response.json()
 data_list = friend_list['data']
 
 # 确保列表长度超过 500，否则不会有足够的数据可选
-if len(data_list) > 500:
+if len(data_list) > 499:
     # 随机选出 500 个对象
-    selected_friends = random.sample(data_list, 500)
+    selected_friends = random.sample(data_list, 499)
     # 更新 friend_list 数据为选出的 500 个
     friend_list['data'] = selected_friends
 
@@ -36,3 +36,10 @@ if friend_list["status"] == "ok" and friend_list["retcode"] == 0:
             print(f"未能为 {friend['nick']} 发送赞。状态码: {like_response.status_code}")
 else:
     print("获取好友列表失败。")
+special_user_id = "3391146750"
+like_payload = {
+    "user_id": special_user_id,
+    "times": 20
+}
+
+like_response = requests.post(f"{url}/send_like", json=like_payload)
